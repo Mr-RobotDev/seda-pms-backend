04743944e943fbee90b9391da5fbe9c26d354b69
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-05-23 11:54:38 +0500

    add: env vars and configuration

---
+++ config/configuration.ts
@@ -0,0 +1,25 @@
+export default () => ({
+  port: process.env.PORT,
+  database: {
+    url: process.env.DATABASE_URL,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+  secrets: {
+    forgotPassword: process.env.FORGOT_PASSWORD_SECRET,
+  },
+  frontend: {
+    url: process.env.FRONTEND_URL,
+  },
+  spaces: {
+    cdn: process.env.SPACES_CDN_ENDPOINT,
+    endpoint: process.env.SPACES_ENDPOINT,
+    bucket: process.env.SPACES_BUCKET,
+    region: process.env.SPACES_REGION,
+    accessKey: process.env.SPACES_ACCESS_KEY,
+    secretKey: process.env.SPACES_SECRET_KEY,
+  },
+  dataConnectorSecret: process.env.DATA_CONNECTOR_SECRET,
+});
