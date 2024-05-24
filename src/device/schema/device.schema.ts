7a87ea2264df541569b1b3ea0307ce67ac0c85b7
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-05-25 00:06:22 +0500

    update: add axis object in device schema

--- src/device/schema/device.schema.ts
+++ src/device/schema/device.schema.ts
@@ -6,17 +6,33 @@ import {
   paginatedAggregation,
 } from '../../common/plugins/pagination.plugin';
 
+class Axis extends Document {
+  @Prop({
+    type: Number,
+    required: true,
+  })
+  x: number;
+
+  @Prop({
+    type: Number,
+    required: true,
+  })
+  y: number;
+}
+
+const AxisSchema = SchemaFactory.createForClass(Axis);
+
 @Schema({
   timestamps: true,
 })
 export class Device extends Document {
   @Prop({
     type: String,
-    required: true,
     unique: true,
     index: true,
+    sparse: true,
   })
-  oem: string;
+  oem?: string;
 
   @Prop({
     type: String,
@@ -32,15 +48,19 @@ export class Device extends Document {
 
   @Prop({
     type: Number,
-    required: true,
   })
-  temperature: number;
+  temperature?: number;
 
   @Prop({
     type: Number,
+  })
+  relativeHumidity?: number;
+
+  @Prop({
+    type: AxisSchema,
     required: true,
   })
-  relativeHumidity: number;
+  axis: Axis;
 }
 
 export const DeviceSchema = SchemaFactory.createForClass(Device);
