f092bafa56deb887cb34294d96fd8e05952c89f5
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-05-27 00:16:11 +0500

    feat: dummy receive pressure device events api

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -5,16 +5,20 @@ import {
   Req,
   HttpCode,
   HttpStatus,
+  Body,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { WebhookService } from './webhook.service';
 import { Public } from '../common/decorators/public.decorator';
 
-@Controller('webhook')
+@Public()
+@Controller({
+  path: 'webhook',
+  version: '1',
+})
 export class WebhookController {
   constructor(private readonly webhookService: WebhookService) {}
 
-  @Public()
   @Post('receive-events')
   @HttpCode(HttpStatus.OK)
   receiveEvents(
@@ -22,5 +26,11 @@ export class WebhookController {
     @Headers('x-dt-signature') signature: string,
   ) {
     return this.webhookService.receiveEvents(request.body, signature);
+  }
+
+  @Post('receive-pressure-events')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEvents(@Body() body: any) {
+    console.log('Pressure events received', body);
   }
 }
