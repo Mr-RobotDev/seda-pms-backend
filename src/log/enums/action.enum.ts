1f2c8c6515aad80a063ba5ee2e45c31fb21aa551
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-05-27 22:15:33 +0500

    add: action and page enums

---
+++ src/log/enums/action.enum.ts
@@ -0,0 +1,10 @@
+export enum Action {
+  LOGGED_IN = 'logged in',
+  LOGGED_OUT = 'logged out',
+  CREATED = 'created',
+  VIEWED = 'viewed',
+  UPDATED = 'updated',
+  DELETED = 'deleted',
+}
+
+export const ActionValues = Object.values(Action);
---
+++ src/log/enums/page.enum.ts
@@ -0,0 +1,10 @@
+export enum Page {
+  DASHBOARD = 'dashboard',
+  FLOOR_PLAN = 'floor plan',
+  ALERTS = 'alerts',
+  DATA_SOURCES = 'data sources',
+  DEVICES = 'devices',
+  DEVICE = 'device',
+}
+
+export const PageValues = Object.values(Page);
