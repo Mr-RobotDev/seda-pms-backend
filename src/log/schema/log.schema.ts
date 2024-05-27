a82db57ca1a57de2601fcc85e497860b2e3adf97
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-05-27 22:15:42 +0500

    add: log schema

---
+++ src/log/schema/log.schema.ts
@@ -0,0 +1,54 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { User } from '../../user/schema/user.schema';
+import { Device } from '../../device/schema/device.schema';
+import { Action, ActionValues } from '../enums/action.enum';
+import { Page, PageValues } from '../enums/page.enum';
+import toJSON from '../../common/plugins/toJSON.plugin';
+import {
+  paginate,
+  paginatedAggregation,
+} from '../../common/plugins/pagination.plugin';
+
+@Schema({
+  timestamps: true,
+})
+export class Log extends Document {
+  @Prop({
+    type: String,
+    required: true,
+    enum: ActionValues,
+  })
+  action: Action;
+
+  @Prop({
+    type: String,
+    enum: PageValues,
+  })
+  page: Page;
+
+  @Prop({
+    type: String,
+  })
+  userAgent?: string;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: Device.name,
+  })
+  device?: Device;
+
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: User.name,
+    index: true,
+  })
+  user: User;
+}
+
+export const LogSchema = SchemaFactory.createForClass(Log);
+
+LogSchema.plugin(toJSON);
+LogSchema.plugin(paginate);
+LogSchema.plugin(paginatedAggregation);
