25df0f7187e9b31ee25411f7db9e18fed8e6eb22
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-06-04 00:02:07 +0500

    add: timeframe enum

---
+++ src/report/enums/timeframe.enum.ts
@@ -0,0 +1,11 @@
+export enum TimeFrame {
+  TODAY = 'today',
+  YESTERDAY = 'yesterday',
+  THIS_WEEK = 'this_week',
+  LAST_WEEK = 'last_week',
+  LAST_3_DAYS = 'last_3_days',
+  LAST_7_DAYS = 'last_7_days',
+  LAST_30_DAYS = 'last_30_days',
+}
+
+export const TimeFrameValues = Object.values(TimeFrame);
