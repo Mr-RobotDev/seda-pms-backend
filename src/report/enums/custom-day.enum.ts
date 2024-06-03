a4c58cc61620c446a8495c178d5925417accb6a3
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-06-03 23:09:12 +0500

    add: custom day and schedule type enum

---
+++ src/report/enums/custom-day.enum.ts
@@ -0,0 +1,11 @@
+export enum CustomDay {
+  MONDAY = 'monday',
+  TUESDAY = 'tuesday',
+  WEDNESDAY = 'wednesday',
+  THURSDAY = 'thursday',
+  FRIDAY = 'friday',
+  SATURDAY = 'saturday',
+  SUNDAY = 'sunday',
+}
+
+export const CustomDayValues = Object.values(CustomDay);
---
+++ src/report/enums/schedule-type.enum.ts
@@ -0,0 +1,7 @@
+export enum ScheduleType {
+  EVERYDAY = 'everyday',
+  WEEKDAYS = 'weekdays',
+  CUSTOM = 'custom',
+}
+
+export const ScheduleTypeValues = Object.values(ScheduleType);
