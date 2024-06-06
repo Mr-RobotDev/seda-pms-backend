70ded6bb8dda34a212588d96bd905b9ddf1e349e
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-06-06 15:07:07 +0500

    feat: receive pressure events api per device

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -28,9 +28,73 @@ export class WebhookController {
     return this.webhookService.receiveEvents(request.body, signature);
   }
 
-  @Post('receive-pressure-events')
+  @Post('receive-pressure-events-g05')
   @HttpCode(HttpStatus.OK)
-  receivePressureEvents(@Req() req: RawBodyRequest<Request>) {
+  receivePressureEventsG05(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g07')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG07(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g08')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG08(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g23')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG23(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g26')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG26(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g28')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG28(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g41')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG41(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g37')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG37(@Req() req: RawBodyRequest<Request>) {
+    const rawBodyBuffer = req.rawBody;
+    const rawBodyString = rawBodyBuffer.toString('utf8');
+    console.log('Pressure events received', rawBodyString);
+  }
+
+  @Post('receive-pressure-events-g33')
+  @HttpCode(HttpStatus.OK)
+  receivePressureEventsG33(@Req() req: RawBodyRequest<Request>) {
     const rawBodyBuffer = req.rawBody;
     const rawBodyString = rawBodyBuffer.toString('utf8');
     console.log('Pressure events received', rawBodyString);
