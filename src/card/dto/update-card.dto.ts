aadb07b5124f0160d12c2551aba565d2e74fa719
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-07-03 22:50:50 +0500

    update: update card dto not to omit devices

--- src/card/dto/update-card.dto.ts
+++ src/card/dto/update-card.dto.ts
@@ -1,6 +1,4 @@
-import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateCardDto } from './create-card.dto';
 
-export class UpdateCardDto extends PartialType(
-  OmitType(CreateCardDto, ['devices']),
-) {}
+export class UpdateCardDto extends PartialType(CreateCardDto) {}
